e796f67ea535a040143f6cf2240593c8013ee39c
Teimuran <81552237+Teimuran@users.noreply.github.com>
2021-03-30 09:14:48 +0300

    galaxy

--- files.py
+++ files.py
@@ -5,54 +5,89 @@ Created on Sun Mar 28 13:41:06 2021
 @author: IQ
 """
 
+
 import os
 import dlib
 import cv2
-from scipy.spatial import distance
-
-
+from scipy.spatial import distance  #импорт
 
 stars = []
-d=[]
+d=[]            #создание пустых массивов
 dets=[]
 
 def getDescriptor(img):
-    descriptors =[]
-    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    dets=detector(img, 1)
-    for num, face in enumerate(dets):
+    descriptors =[]   #создание пустого массива
+    
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)   #преобразование из GBR в RGB 
+    
+    dets=detector(img, 1)                
+    for num, face in enumerate(dets):      #цикл for где записываются все координаты лиц
+    
         shape=sp(img, face)
+        
         face_descriptor = facerec.compute_face_descriptor(img,shape)
+        
         descriptors.append(face_descriptor)
+    
     return descriptors    
         
 os.chdir('C:/galaxy')
 
-predictor_path = 'shape_predictor_5_face_landmarks.dat'
+predictor_path = 'shape_predictor_5_face_landmarks.dat'   #импорт моделей распознавания
 face_rec_model_path = 'dlib_face_recognition_resnet_model_v1.dat'
 
-detector = dlib.get_frontal_face_detector()
+detector = dlib.get_frontal_face_detector()      #создание переменных с моделями распознавания
 sp = dlib.shape_predictor(predictor_path)
 facerec = dlib.face_recognition_model_v1(face_rec_model_path)
 
-os.chdir('C:/galaxy') 
-
-img = cv2.imread('7.jpg')
-desk = getDescriptor(img)
-
-# print(desk)
+###############################################################
 
 os.chdir('C:/galaxy/dataset')
-stars = []
+stars = []    #создание пустого массива
+
 for _stars in os.listdir():
     print(os.getcwd())
-    os.chdir(_stars)
-    for name in os.listdir():
-        print(os.getcwd()+'/'+name)
+
+    os.chdir(_stars)  
+    stars.append((_stars))
+
+    pic = []   #создание пустого массива
+
+    for name in os.listdir():         #цикл for с выводом полного пути файла
+        print(os.getcwd()+'\ '+name)
+
         img = cv2.imread(name)
-        stars.append(getDescriptor(img))
-        
+
+        pic.append(getDescriptor(img))   #получение дескрипторов с каждого фото из датасета
+
+    stars.append(pic)    
     os.chdir('..')
      
-print(len(stars))
+
+maximum = 1
+
+os.chdir('C:/galaxy')
+
+w=cv2.imread('123.jpg')
+pos = 0
+
+desc = getDescriptor(w)
+
+for pos, star in enumerate(stars):  #цикл сравнения с датасетом и выбором лучшего результата
+
+    if pos % 2 == 1:
+
+        for pic in star:
+            delta = distance.euclidean(desc, pic)
+            print(delta)
+            res = delta
+
+            if res < maximum:
+                maximum = res
+                ind = pos
+            else:
+                ind = ind
+print(maximum, stars[ind-1])
+
+
 
